6b6defb662163df6c89a30946a0407d9ba990034
Christopher Peters <petersc@nskind.com>
2026-09-06 12:48:41 -0400

    worktext

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,27 @@
-Consent to get work texts
+export default function NotesPage() {
+  return (
+    <main
+      style={{
+        maxWidth: "800px",
+        margin: "0 auto",
+        padding: "2rem",
+        fontFamily: "Arial, sans-serif",
+        lineHeight: 1.6,
+      }}
+    >
+      <p>
+        <strong>Consent to get work texts</strong>
+      </p>
 
-You will receive texts when a new issue is submitted to the local intranet form.  Your number will not be shared or published anywhere - this is a strictly internal communication for NSK.  You may receive several texts in a day, however most days will not have any.  Once the software is running properly this program will be eliminated.
+      <p>
+        You will receive texts when a new issue is submitted to the local
+        intranet form. Your number will not be shared or published anywhere.
+        This is a strictly internal communication for NSK. You may receive
+        several texts in a day; however, most days will not have any. Once the
+        software is running properly, this program will be eliminated.
+      </p>
 
-Messages and data rates may apply
+      <p>Messages and data rates may apply.</p>
+    </main>
+  );
+}
