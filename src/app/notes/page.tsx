9364ddad34288c5ba8239f262d6f0836d60ad195
Christopher Peters <petersc@nskind.com>
2026-09-06 12:45:56 -0400

    worktext

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,5 @@
+Consent to get work texts
+
+You will receive texts when a new issue is submitted to the local intranet form.  Your number will not be shared or published anywhere - this is a strictly internal communication for NSK.  You may receive several texts in a day, however most days will not have any.  Once the software is running properly this program will be eliminated.
+
+Messages and data rates may apply
